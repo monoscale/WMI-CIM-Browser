b4ae2ff2d0021631541c374a598cb96b5cbb71bd
Bert De Saffel <bertdesaffel@gmail.com>
2018-10-28 22:32:06 +0100

    documentation

--- WMI CIM Browser/MainWindow.xaml.cs
+++ WMI CIM Browser/MainWindow.xaml.cs
@@ -20,25 +20,111 @@ namespace WMI_CIM_Browser {
         private WbemService service;
 
         private SearchClassResultWindow searchClassResultWindow;
-
+    
         public MainWindow() {
             InitializeComponent();
 
             locator = new WbemLocator();
             WindowState = WindowState.Maximized;
 
-            PopulateClassNavigator("root");
+            PopulateClassNavigator("root");    
         }
 
+
+        #region EVENTS
         /// <summary>
         /// Get the classes from a namespace
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void TextBoxNamespace_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
             if (e.Key != System.Windows.Input.Key.Enter) { return; }
             PopulateClassNavigator(TextBoxNamespace.Text);
         }
+
+        /// <summary>
+        /// Give extra detail for the selected WMI Class
+        /// </summary>
+        private void ClassNavigator_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            if (ClassNavigator.SelectedItem == null) {
+                //it just means the user went out of focus
+                return;
+            }
+            PopulateClassDetails(ClassNavigator.SelectedItem);
+        }
+
+
+        /// <summary>
+        /// Starts searching for classes when enter button is pressed. 
+        /// Once the search is done, it will open a new SearchClassResultWindow Window with the results.
+        /// </summary>
+        private void TextBoxClass_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key != System.Windows.Input.Key.Enter) { return; }
+            IList<WbemTreeViewItem> classes = ClassNavigator.Search(TextBoxClass.Text);
+            searchClassResultWindow = new SearchClassResultWindow(classes);
+            searchClassResultWindow.ClassNameList.MouseDoubleClick += ClassNameList_MouseDoubleClick;
+            searchClassResultWindow.Show();
+        }
+
+        /// <summary>
+        /// When double clicking in the list of classnames from the search operation (in the SearchClassResultWindow),
+        /// we retrieve the classname and load the details. The SearchClassResultWindow will also be closed.
+        /// </summary>
+        private void ClassNameList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            ListBox listBox = (ListBox)sender;
+            ListBoxItem listBoxItem = (ListBoxItem)listBox.SelectedItem;
+            WbemTreeViewItem item = (WbemTreeViewItem)listBoxItem.DataContext;
+            PopulateClassDetails(item);
+            searchClassResultWindow.Close();
+            searchClassResultWindow = null;
+        }
+
+
+        /// <summary>
+        /// When right clicking on the current classname, give a menu to interact with that class.
+        /// </summary>
+        private void TextBlockCurrentClass_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            ContextMenu contextMenu = new ContextMenu();
+
+            MenuItem showQualifiers = new MenuItem();
+            showQualifiers.Header = "Show Class Qualifiers";
+            showQualifiers.Click += ShowClassQualifiers;
+            contextMenu.Items.Add(showQualifiers);
+
+            MenuItem showInstances = new MenuItem();
+            showInstances.Header = "Show Class Instances";
+            showInstances.Click += ShowClassInstances;
+            contextMenu.Items.Add(showInstances);
+
+            contextMenu.IsOpen = true;
+        }
+
+        private void PropertyList_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e) {
+            // individual attribute qualifiers
+
+        }
+
+        /// <summary>
+        /// Shows the instances of a class. This can only be called when a current class is selected.
+        /// We also know which class this (The selected item in ClassNavigator).
+        /// </summary>
+        private void ShowClassInstances(object sender, RoutedEventArgs e) {
+            WbemObject mObject = ClassNavigator.SelectedItem.DataContext;
+            IList<WbemObject> instances = service.InstancesOf(mObject.Path.ClassName);
+            ExtraDetails.ItemsSource = instances;
+        }
+
+        /// <summary>
+        /// Shows the class qualifiers of a class. This can only be called when a current class is selected.
+        /// We also know which class this (The selected item in ClassNavigator).
+        /// </summary>
+        private void ShowClassQualifiers(object sender, RoutedEventArgs e) {
+            WbemObject mObject = ClassNavigator.SelectedItem.DataContext;
+            IList<DataGridClassQualifierView> qualifiers = mObject.Qualifiers.Values.Select(s => new DataGridClassQualifierView(s)).ToList();
+            ExtraDetails.ItemsSource = qualifiers;
+        }
+        #endregion
+
+
+        #region HELPER METHODS
 
         /// <summary>
         /// This method populates the ClassNavigator treeview with all the WMI classes belonging to the specified namespace.
@@ -56,21 +142,10 @@ namespace WMI_CIM_Browser {
             }
         }
 
-
-        /// <summary>
-        /// Give extra detail for the selected WMI Class
+                /// <summary>
+        /// Populates the properties and methods from a class that is represented by a wbemTreeViewItem.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ClassNavigator_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
-            if (ClassNavigator.SelectedItem == null) {
-                //it just means the user went out of focus
-                return;
-            }
-            PopulateClassDetails(ClassNavigator.SelectedItem);
-
-        }
-
+        /// <param name="wbemTreeViewItem">The WbemTreeViewItem that represents the class</param>
         private void PopulateClassDetails(WbemTreeViewItem wbemTreeViewItem) {
 
             PropertyList.ItemsSource = null;
@@ -86,58 +161,7 @@ namespace WMI_CIM_Browser {
             PropertyList.ItemsSource = properties;
             MethodList.ItemsSource = methods;
         }
-
-        private void TextBoxClass_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
-            if (e.Key != System.Windows.Input.Key.Enter) { return; }
-            IList<WbemTreeViewItem> classes = ClassNavigator.Search(TextBoxClass.Text);
-            searchClassResultWindow = new SearchClassResultWindow(classes);
-            searchClassResultWindow.ClassNameList.MouseDoubleClick += ClassNameList_MouseDoubleClick;
-            searchClassResultWindow.Show();
-        }
-
-        private void ClassNameList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            ListBox listBox = (ListBox)sender;
-            ListBoxItem listBoxItem = (ListBoxItem)listBox.SelectedItem;
-            WbemTreeViewItem item = (WbemTreeViewItem)listBoxItem.DataContext;
-            PopulateClassDetails(item);
-            searchClassResultWindow.Close();
-            searchClassResultWindow = null;
-
-
-        }
-
-        private void PropertyList_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e) {
-            // individual attribute qualifiers
-
-        }
-
-        private void TextBlockCurrentClass_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            ContextMenu contextMenu = new ContextMenu();
-
-            MenuItem showQualifiers = new MenuItem();
-            showQualifiers.Header = "Show Class Qualifiers";
-            showQualifiers.Click += ShowClassQualifiers;
-            contextMenu.Items.Add(showQualifiers);
-
-            MenuItem showInstances = new MenuItem();
-            showInstances.Header = "Show Class Instances";
-            showInstances.Click += ShowClassInstances;
-            contextMenu.Items.Add(showInstances);
-
-            contextMenu.IsOpen = true;
-        }
-
-        private void ShowClassInstances(object sender, RoutedEventArgs e) {
-            WbemObject mObject = ClassNavigator.SelectedItem.DataContext;
-            IList<WbemObject> instances = service.InstancesOf(mObject.Path.ClassName);
-            ExtraDetails.ItemsSource = instances;
-        }
-
-        private void ShowClassQualifiers(object sender, RoutedEventArgs e) {
-            WbemObject mObject = ClassNavigator.SelectedItem.DataContext;
-            IList<DataGridClassQualifierView> qualifiers = mObject.Qualifiers.Values.Select(s => new DataGridClassQualifierView(s)).ToList();
-            ExtraDetails.ItemsSource = qualifiers;
-        }
+        #endregion
 
 
     }
